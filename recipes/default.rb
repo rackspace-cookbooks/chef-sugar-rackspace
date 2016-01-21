56ace2e788ebe063edd4a9abfba05aa92169c380
Jim Rosser <jarosser06@gmail.com>
2016-01-21 07:47:08 -0600

    Fixed Chef 12 warning by setting compile_time true if supported

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,10 @@
 
 gem_version = run_context.cookbook_collection[cookbook_name].metadata.version
 
-chef_gem('chef-sugar-rackspace') do
+chef_gem 'chef-sugar-rackspace' do
   version gem_version
-  action :nothing
-end.run_action(:install)
+  compile_time true if Chef::Resource::ChefGem.instance_methods(false)
+                                              .include?(:compile_time)
+end
 
 require 'chef/sugar/rackspace'
